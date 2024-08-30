b47034340322e2f25e067a68192c03223909b3f1
sagar <sagarsharmatech00@gmail.com>
2024-08-30 13:22:32 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-row items-center justify-between p-24 gap-3 flex-wrap">
       {
           // [1,2,3,4,5,6,7,8 ,9,0]
-      programming_tutorials.slice(0,10).map((ele, key) => (
+      programming_tutorials.slice(0,11).map((ele, key) => (
         
         <div key={key}>
           <Card data={ele} />
